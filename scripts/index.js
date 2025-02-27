e5c3c59d16e273669606a027d7fc90811f32ae5c
mdmhrz <razufreelence@gmail.com>
2025-02-27 20:55:41 +0600

    Check commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 document.getElementById('select1')
     .addEventListener('click', function () {
-        document.getElementById('banner').style.backgroundImage = "url('../media/horizon.png')";
+        document.getElementById('banner').style.backgroundImage = "url('./media/horizon.png')";
         document.getElementById('select1').style.height = '5px';
         document.getElementById('select2').style.height = '2px';
         document.getElementById('select3').style.height = '2px';
@@ -13,7 +13,7 @@ document.getElementById('select1')
 
 document.getElementById('select2')
     .addEventListener('click', function () {
-        document.getElementById('banner').style.backgroundImage = "url('../media/horizon-2.jpg')";
+        document.getElementById('banner').style.backgroundImage = "url('./media/horizon-2.jpg')";
         document.getElementById('select2').style.height = '5px';
         document.getElementById('select1').style.height = '2px';
         document.getElementById('select3').style.height = '2px';
@@ -27,7 +27,7 @@ document.getElementById('select2')
 
 document.getElementById('select3')
     .addEventListener('click', function () {
-        document.getElementById('banner').style.backgroundImage = "url('../media/horizon-3.jpg')";
+        document.getElementById('banner').style.backgroundImage = "url('./media/horizon-3.jpg')";
         document.getElementById('select3').style.height = '5px';
         document.getElementById('select1').style.height = '2px';
         document.getElementById('select2').style.height = '2px';
